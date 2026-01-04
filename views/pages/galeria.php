2ff05c8a49702e06128b9df32476f5f826b79937
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2026-01-04 15:07:12 -0600

    feat: Add new gallery page to display montajes with image carousels.

--- views/pages/galeria.php
+++ views/pages/galeria.php
@@ -8,48 +8,52 @@
 <div class="container-fluid container-service py-5">
     <div class="container py-5">
         <div class="row">
-            <?php foreach ($montajes as $montaje): ?>
-                <div class="col-md-4">
-                    <div class="card wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
-                        <div id="carouselExampleIndicators<?= $montaje->id ?>" class="carousel slide"
-                            data-bs-ride="carousel">
-                            <div class="carousel-indicators">
-                                <?php foreach ($montaje->fotos as $key => $foto): ?>
-                                    <button type="button" data-bs-target="#carouselExampleIndicators<?= $montaje->id ?>"
-                                        data-bs-slide-to="<?= $key ?>" class="active" aria-current="true"
-                                        aria-label="Slide 1"></button>
-                                <?php endforeach; ?>
+            <?php if (is_array($montajes) && count($montajes) > 0): ?>
+                <?php foreach ($montajes as $montaje): ?>
+                    <div class="col-md-4">
+                        <div class="card wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                            <div id="carouselExampleIndicators<?= $montaje->id ?>" class="carousel slide"
+                                data-bs-ride="carousel">
+                                <div class="carousel-indicators">
+                                    <?php foreach ($montaje->fotos as $key => $foto): ?>
+                                        <button type="button" data-bs-target="#carouselExampleIndicators<?= $montaje->id ?>"
+                                            data-bs-slide-to="<?= $key ?>" class="active" aria-current="true"
+                                            aria-label="Slide 1"></button>
+                                    <?php endforeach; ?>
+                                </div>
+                                <div class="carousel-inner">
+                                    <?php foreach ($montaje->fotos as $key => $foto): ?>
+                                        <div class="carousel-item <?= $key == 0 ? 'active' : '' ?>">
+                                            <img src="<?= $_ENV['MONTAJES_URL'] . $foto ?>" class="d-block w-100" alt="...">
+                                        </div>
+                                    <?php endforeach; ?>
+                                </div>
+                                <button class="carousel-control-prev" type="button"
+                                    data-bs-target="#carouselExampleIndicators<?= $montaje->id ?>" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button"
+                                    data-bs-target="#carouselExampleIndicators<?= $montaje->id ?>" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
                             </div>
-                            <div class="carousel-inner">
-                                <?php foreach ($montaje->fotos as $key => $foto): ?>
-                                    <div class="carousel-item <?= $key == 0 ? 'active' : '' ?>">
-                                        <img src="<?= $_ENV['MONTAJES_URL'] . $foto ?>" class="d-block w-100" alt="...">
-                                    </div>
-                                <?php endforeach; ?>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <?= $montaje->titulo ?>
+                                </h5>
+                                <p><span class="badge bg-primary"><?= $montaje->tipo_servicio->nombre ?></span></p>
+                                <p class="card-text">
+                                    <?= $montaje->descripcion ?>
+                                </p>
                             </div>
-                            <button class="carousel-control-prev" type="button"
-                                data-bs-target="#carouselExampleIndicators<?= $montaje->id ?>" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button"
-                                data-bs-target="#carouselExampleIndicators<?= $montaje->id ?>" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <?= $montaje->titulo ?>
-                            </h5>
-                            <p><span class="badge bg-primary"><?= $montaje->tipo_servicio->nombre ?></span></p>
-                            <p class="card-text">
-                                <?= $montaje->descripcion ?>
-                            </p>
                         </div>
                     </div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>No hay montajes disponibles</p>
+            <?php endif; ?>
         </div>
     </div>
 </div>
